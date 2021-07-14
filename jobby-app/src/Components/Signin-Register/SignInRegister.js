b3dad991efbb1f0c2e269ff854a3c5159fecf528
ikfreecharles <sleektrends7@gmail.com>
2021-07-14 21:31:38 +0200

    update signin-register page logic

--- jobby-app/src/Components/Signin-Register/SignInRegister.js
+++ jobby-app/src/Components/Signin-Register/SignInRegister.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import SignIn from "./Sign-in/SignIn";
 import Register from "./Register/Register";
 
 function SignInRegister() {
+   const [Option, setOption] = useState("signin");
+   const handleClick = (option) => {
+      if (option === "signin") {
+         setOption("signin");
+      } else if (option === "register") {
+         setOption("register");
+      }
+   };
    return (
       <article>
          <div className="sir-inner-container">
             <div className="sir-sign-in-register">
-               <p>Sign In</p>
-               <p>Register</p>
+               <p onClick={() => handleClick("signin")}>Sign In</p>
+               <p onClick={() => handleClick("register")}>Register</p>
             </div>
             <div className="sir-logo">
                <img src="" alt="" />
             </div>
-            <SignIn />
-            <Register />
+            {Option === "signin" && <SignIn />}
+            {Option === "register" && <Register />}
          </div>
       </article>
    );
