d51f563e3858e287182f370234bcf823186daae1
captainPlanto <78704276+captainplanto@users.noreply.github.com>
2021-07-24 14:40:43 +0200

    Updated file for charles

--- jobby-app/src/Components/Nav/Nav.js
+++ jobby-app/src/Components/Nav/Nav.js
@@ -3,66 +3,68 @@ import { Link } from "react-router-dom";
 
 //import components and css
 import "./nav.css";
+import "./navMediaquery.css";
 import { IoNotifications, IoPerson } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
 import ProfileDropdown from "./Profile-Dropdown/ProfileDropdown";
 import Jobby from "../../Images/Jobby.png";
 
 function Nav({ SignInIsTrue, handleSignIn }) {
-   const [ShowProfile, setShowProfile] = useState(false);
+  const [ShowProfile, setShowProfile] = useState(false);
 
-   useEffect(() => {
+  useEffect(() => {
+    setShowProfile(false);
+    return () => {
       setShowProfile(false);
-      return () => {
-         setShowProfile(false);
-      };
-   }, []);
-   return (
-      <section className="container n-container">
-         <div className="n-logo">
-            <img src={Jobby} alt="logo" />
-         </div>
-         <div className="n-menu">
-            <ul>
-               <Link to="/" className="n-jobsearch">
-                  <li>Home</li>
-               </Link>
+    };
+  }, []);
+  return (
+    <section className="container n-container">
+      <div className="n-logo">
+        <img src={Jobby} alt="logo"/>
+      </div>
+      <span class="material-icons">menu</span>
+      <div className="n-menu">
+        <ul>
+          <Link to="/" className="n-jobsearch">
+            <li>Home</li>
+          </Link>
 
-               <Link to="/jobsearch" className="n-jobsearch">
-                  <li>Job Search</li>
-               </Link>
+          <Link to="/jobsearch" className="n-jobsearch">
+            <li>Job Search</li>
+          </Link>
 
-               <li>For Companies</li>
-            </ul>
-         </div>
-         {SignInIsTrue ? (
-            <div className="n-icons">
-               <Link to="/notification" className="n-links">
-                  {" "}
-                  <IoNotifications />
-               </Link>
-               <Link to="/message" className="n-links">
-                  <MdEmail />
-               </Link>
-               <IoPerson
-                  onClick={() => {
-                     setShowProfile((ShowProfile) => !ShowProfile);
-                  }}
-                  className="n-links"
-               />
-               {ShowProfile && <ProfileDropdown handleSignIn={handleSignIn} />}
-            </div>
-         ) : (
-            <Link to="/signin">
-               <div>
-                  <span>SignIn</span>
-                  <span>/</span>
-                  <span>Register</span>
-               </div>
-            </Link>
-         )}
-      </section>
-   );
+          <li>For Companies</li>
+        </ul>
+      </div>
+      {SignInIsTrue ? (
+        <div className="n-icons">
+          <Link to="/notification" className="n-links">
+            {" "}
+            <IoNotifications />
+          </Link>
+          <Link to="/message" className="n-links">
+            <MdEmail />
+          </Link>
+          <IoPerson
+            onClick={() => {
+              setShowProfile((ShowProfile) => !ShowProfile);
+            }}
+            className="n-links"
+          />
+          {ShowProfile && <ProfileDropdown handleSignIn={handleSignIn} />}
+        </div>
+      ) : (
+        <Link to="/signin">
+          <div>
+            <span>SignIn</span>
+            <span>/</span>
+            <span>Register</span>
+          </div>
+        </Link>
+      )}
+    </section>
+  );
 }
 
 export default Nav;
